311eaf3ceae4ce29bd14926048ac51de80dd5e6f
jsanchez <josegsv28@gmail.com>
2020-12-24 00:01:11 -0400

    asies

--- src/views/Taller/AsignationToRefact.js
+++ src/views/Taller/AsignationToRefact.js
@@ -6,17 +6,16 @@ const AsignationToRefact = () => {
     const [data, setData] = React.useState({
 
     });
-    const [toggleDiagnostico, setToggleDiagnostico] = useState(false)
+    const [toggleRefaccion, setToggleRefaccion] = useState(false)
+    const [toggleHerramientas, setToggleHerramientas] = useState(false)
         return (
         <div>
-
-        
-        <MDBContainer fluid xs="12" sm="12" md="12" lg="12" xl="12" style={{verticalAlign:"middle", textAlignLast: "center"}}>
+            <MDBContainer fluid xs="12" sm="12" md="12" lg="12" xl="12" style={{verticalAlign:"middle", textAlignLast: "center"}}>
                     <CardHeader className="mt-2 mb-2">
                              <Row className="ml-4 mt-1 pt-1" style={{textAlign:"center"}}>
                                <Col xs="12" sm="12" md="12" lg="12" xl="12">
                                  <MDBIcon className="mt-1" size="2x" icon="info-circle"/> &nbsp;
-                                 <p style={{fontSize: 25, fontWeight: 'bold'}}>Información:<span style={{fontSize: 25, fontWeight: '300'}}> Seleccione el botón diagnóstico para visualizar el diagnóstico del vehículo en específico, si no posee uno nuestro sistema le mostrará una lista de mecánicos no ocupados para realizar el mismo.</span></p>
+                                 <p style={{fontSize: 25, fontWeight: 'bold'}}>Información:<span style={{fontSize: 25, fontWeight: '300'}}> Seleccione el botón refacción o herramientas para visualizar las que fueron asignadas al vehículo en específico, si no posee alguno nuestro sistema le ayudará a asignar los mismos.</span></p>
                                </Col>
                              </Row>
                   </CardHeader>
@@ -28,7 +27,8 @@ const AsignationToRefact = () => {
                     <th>Cédula</th>
                     <th>Color</th>
                     <th>Placa</th>
-                    <th>Diagnóstico</th>
+                    <th>Refacción</th>
+                    <th>Herramientas</th>
                     </tr>
                 </MDBTableHead>
                 <MDBTableBody>
@@ -38,7 +38,8 @@ const AsignationToRefact = () => {
                     <td>26131991</td>
                     <td>Rojo</td>
                     <td>RAP 44W</td>
-                    <td><Button onClick={() => setToggleDiagnostico(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-wrench fa-lg" style={{color: "green"}}></i></Button></td>
+                    <td><Button onClick={() => setToggleRefaccion(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-hand-lizard-o fa-lg" style={{color: "green"}}></i></Button></td>
+                    <td><Button onClick={() => setToggleHerramientas(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-wrench fa-lg" style={{color: "green"}}></i></Button></td>
                     </tr>
                     <tr>
                     <td>Simón Antonio</td>
@@ -46,59 +47,123 @@ const AsignationToRefact = () => {
                     <td>27445668</td>
                     <td>Vinotinto</td>
                     <td>AE05CG</td>
-                    <td><Button onClick={() => setToggleDiagnostico(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-wrench fa-lg" style={{color: "green"}}></i></Button></td>                    </tr>
+                    <td><Button onClick={() => setToggleRefaccion(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-hand-lizard-o fa-lg" style={{color: "green"}}></i></Button></td>
+                    <td><Button onClick={() => setToggleHerramientas(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-wrench fa-lg" style={{color: "green"}}></i></Button></td>                  </tr>
                     <tr>
                     <td>Luis Alexander</td>
                     <td>Isturiz Medina</td>
                     <td>25368515</td>
                     <td>Verde</td>
                     <td>RAP 44W</td>
-                    <td><Button onClick={() => setToggleDiagnostico(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-wrench fa-lg" style={{color: "green"}}></i></Button></td>                    </tr>
+                    <td><Button onClick={() => setToggleRefaccion(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-hand-lizard-o fa-lg" style={{color: "green"}}></i></Button></td>
+                    <td><Button onClick={() => setToggleHerramientas(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-wrench fa-lg" style={{color: "green"}}></i></Button></td>                    </tr>
                     <tr>
                     <td>Jesibell Andreina</td>
                     <td>Sánchez Valderrama</td>
                     <td>26131988</td>
                     <td>Morado</td>
                     <td>VCG 77K</td>
-                    <td><Button onClick={() => setToggleDiagnostico(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-wrench fa-lg" style={{color: "green"}}></i></Button></td>                    </tr>
+                    <td><Button onClick={() => setToggleRefaccion(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-hand-lizard-o fa-lg" style={{color: "green"}}></i></Button></td>
+                    <td><Button onClick={() => setToggleHerramientas(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-wrench fa-lg" style={{color: "green"}}></i></Button></td>                   </tr>
                 </MDBTableBody>
             </MDBTable>
         </MDBContainer>
-        <MDBContainer>
-            <MDBModal isOpen={toggleDiagnostico}>
-              <MDBModalHeader>Diagnóstico</MDBModalHeader>
-                <MDBModalBody>
-                        <Row className="mt-2 mb-1" style={{textAlign:"center", placeContent:"center"}}>
-                            <Col xs="12" sm="12" md="8" lg="8" xl="8" style={{textAlignLast:"center"}}>
-                              <FormGroup>
-                                <Label htmlFor="t_user"><MDBIcon className="mt-1" size="lg" icon="user"/> Selecciona el mecánico que hará el diagnóstico</Label>
-                                <Input
-                                  type="select" 
-                                  id="t_user" 
-                                  placeholder="Selecciona el usuario al que deseas actualizar" 
-                                  onChange=""
-                                  value=""
-                                  required
-                                >
-                                  <option value="">Mecánicos disponibles</option>
-                                  <option value="1">26131991 - José Sánchez</option>
-                                  <option value="2">27556887 - Emmanuel Zambrano</option>
-                                  <option value="3">25445796 - Ángel Vargas</option>
-                                </Input>
-                              </FormGroup>
-                            </Col>
+            <MDBContainer>
+                <MDBModal isOpen={toggleRefaccion}>
+                    <MDBModalHeader style={{justifyContent:"center"}}>
+                        <Row>
+                            Refacción
                         </Row>
-                </MDBModalBody>
-                <MDBModalFooter>
-                    <MDBBtn color='info' style={{color : "white"}}>
-                      Aceptar
+                    </MDBModalHeader>
+                    <MDBModalBody>
+                            <Row className="mt-2 mb-1" style={{textAlign:"center", placeContent:"center"}}>
+                                <Col xs="12" sm="12" md="8" lg="8" xl="8" style={{textAlignLast:"center"}}>
+                                  <FormGroup>
+                                    <Label htmlFor="t_user"><MDBIcon className="mt-1" size="lg" icon="user"/> Selecciona un mecánico disponible</Label>
+                                    <Input
+                                      type="select" 
+                                      id="t_user" 
+                                      placeholder="Selecciona el usuario al que deseas actualizar" 
+                                      onChange=""
+                                      value=""
+                                      required
+                                    >
+                                      <option value="">Mecánicos disponibles</option>
+                                      <option value="1">26131991 - José Sánchez</option>
+                                      <option value="2">27556887 - Emmanuel Zambrano</option>
+                                      <option value="3">25445796 - Ángel Vargas</option>
+                                    </Input>
+                                  </FormGroup>
+                                </Col>
+                            </Row>
+                    </MDBModalBody>
+                    <MDBModalFooter>
+                        <MDBBtn color='info' style={{color : "white"}}>
+                          Aceptar
+                        </MDBBtn>
+                      <MDBBtn color='danger'  onClick={() => setToggleRefaccion(false)}>
+                      Cerrar
                     </MDBBtn>
-                  <MDBBtn color='danger'  onClick={() => setToggleDiagnostico(false)}>
-                  Cerrar
-                </MDBBtn>
-              </MDBModalFooter>
-            </MDBModal>
-          </MDBContainer>   
+                  </MDBModalFooter>
+                </MDBModal>
+          </MDBContainer>
+          <MDBContainer>
+                <MDBModal isOpen={toggleHerramientas}>
+                    <MDBModalHeader style={{justifyContent:"center"}}>
+                        <Row>
+                            Herramientas
+                        </Row>
+                    </MDBModalHeader>
+                    <MDBModalBody>
+                            <Row className="mt-2 mb-1" style={{textAlign:"center", placeContent:"center"}}>
+                                <Col xs="12" sm="12" md="8" lg="8" xl="8" style={{textAlignLast:"center", fontSize:"12"}}>
+                                  <FormGroup style={{fontSize:"12"}}>
+                                    <Label style={{fontSize:"12"}} htmlFor="t_user"><MDBIcon className="mt-1" size="lg" icon="wrench"/> Selecciona una herramienta disponible</Label>
+                                    <Input
+                                      type="select" 
+                                      id="t_user" 
+                                      placeholder="Selecciona el usuario al que deseas actualizar" 
+                                      onChange=""
+                                      value=""
+                                      required
+                                    >
+                                      <option value="">Herramientas disponibles</option>
+                                      <option value="1">20 - Gatos Hidráulicos</option>
+                                      <option value="2">14 - Llaves de cruz</option>
+                                      <option value="3">10 - Osciloscopios</option>
+                                    </Input>
+                                  </FormGroup>
+                                </Col>
+                                <Col xs="12" sm="12" md="4" lg="4" xl="4" style={{textAlignLast:"center", fontSize:"13"}}>
+                                  <FormGroup style={{fontSize:"12"}}>
+                                    <Label style={{fontSize:"12"}} htmlFor="t_user"><MDBIcon className="mt-1" size="lg" icon="sort-numeric-up"/> Seleccione cantidad</Label>
+                                    <Input
+                                      type="select" 
+                                      id="t_user" 
+                                      placeholder="Selecciona el usuario al que deseas actualizar" 
+                                      onChange=""
+                                      value=""
+                                      required
+                                    >
+                                      <option value="">cantidad</option>
+                                      <option value="1">1</option>
+                                      <option value="2">2</option>
+                                      <option value="3">3</option>
+                                    </Input>
+                                  </FormGroup>
+                                </Col>
+                            </Row>
+                    </MDBModalBody>
+                    <MDBModalFooter>
+                        <MDBBtn color='info' style={{color : "white"}}>
+                          Aceptar
+                        </MDBBtn>
+                      <MDBBtn color='danger'  onClick={() => setToggleHerramientas(false)}>
+                      Cerrar
+                    </MDBBtn>
+                  </MDBModalFooter>
+                </MDBModal>
+          </MDBContainer>  
         </div>
   );
 }
--- src/views/Taller/Diagnostic.js
+++ src/views/Taller/Diagnostic.js
@@ -9,8 +9,6 @@ const CarPeerClient = () => {
     const [toggleDiagnostico, setToggleDiagnostico] = useState(false)
         return (
         <div>
-
-        
         <MDBContainer fluid xs="12" sm="12" md="12" lg="12" xl="12" style={{verticalAlign:"middle", textAlignLast: "center"}}>
                     <CardHeader className="mt-2 mb-2">
                              <Row className="ml-4 mt-1 pt-1" style={{textAlign:"center"}}>
@@ -28,8 +26,7 @@ const CarPeerClient = () => {
                     <th>Cédula</th>
                     <th>Color</th>
                     <th>Placa</th>
-                    <th>Refacción</th>
-                    <th>Herramientas</th>
+                    <th>Diagnóstico</th>
                     </tr>
                 </MDBTableHead>
                 <MDBTableBody>
@@ -39,7 +36,6 @@ const CarPeerClient = () => {
                     <td>26131991</td>
                     <td>Rojo</td>
                     <td>RAP 44W</td>
-                    <td><Button onClick={() => setToggleDiagnostico(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-hand-lizard-o fa-lg" style={{color: "green"}}></i></Button></td>
                     <td><Button onClick={() => setToggleDiagnostico(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-wrench fa-lg" style={{color: "green"}}></i></Button></td>
                     </tr>
                     <tr>
@@ -48,15 +44,13 @@ const CarPeerClient = () => {
                     <td>27445668</td>
                     <td>Vinotinto</td>
                     <td>AE05CG</td>
-                    <td><Button onClick={() => setToggleDiagnostico(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-hand-lizard-o fa-lg" style={{color: "green"}}></i></Button></td>
-                    <td><Button onClick={() => setToggleDiagnostico(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-wrench fa-lg" style={{color: "green"}}></i></Button></td>                  </tr>
+                    <td><Button onClick={() => setToggleDiagnostico(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-wrench fa-lg" style={{color: "green"}}></i></Button></td>                    </tr>
                     <tr>
                     <td>Luis Alexander</td>
                     <td>Isturiz Medina</td>
                     <td>25368515</td>
                     <td>Verde</td>
                     <td>RAP 44W</td>
-                    <td><Button onClick={() => setToggleDiagnostico(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-hand-lizard-o fa-lg" style={{color: "green"}}></i></Button></td>
                     <td><Button onClick={() => setToggleDiagnostico(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-wrench fa-lg" style={{color: "green"}}></i></Button></td>                    </tr>
                     <tr>
                     <td>Jesibell Andreina</td>
@@ -64,19 +58,22 @@ const CarPeerClient = () => {
                     <td>26131988</td>
                     <td>Morado</td>
                     <td>VCG 77K</td>
-                    <td><Button onClick={() => setToggleDiagnostico(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-hand-lizard-o fa-lg" style={{color: "green"}}></i></Button></td>
-                    <td><Button onClick={() => setToggleDiagnostico(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-wrench fa-lg" style={{color: "green"}}></i></Button></td>                   </tr>
+                    <td><Button onClick={() => setToggleDiagnostico(true)} color="primary" style={{backgroundColor: "transparent", borderColor: "transparent"}}><i className="fa fa-wrench fa-lg" style={{color: "green"}}></i></Button></td>                    </tr>
                 </MDBTableBody>
             </MDBTable>
         </MDBContainer>
         <MDBContainer>
             <MDBModal isOpen={toggleDiagnostico}>
-              <MDBModalHeader>Diagnóstico</MDBModalHeader>
+                <MDBModalHeader style={{justifyContent:"center"}}>
+                    <Row>
+                        Diagnóstico
+                    </Row>
+                </MDBModalHeader>
                 <MDBModalBody>
                         <Row className="mt-2 mb-1" style={{textAlign:"center", placeContent:"center"}}>
                             <Col xs="12" sm="12" md="8" lg="8" xl="8" style={{textAlignLast:"center"}}>
                               <FormGroup>
-                                <Label htmlFor="t_user"><MDBIcon className="mt-1" size="lg" icon="user"/> Selecciona un mecánico disponible</Label>
+                                <Label htmlFor="t_user"><MDBIcon className="mt-1" size="lg" icon="user"/> Selecciona el mecánico que hará el diagnóstico</Label>
                                 <Input
                                   type="select" 
                                   id="t_user" 
@@ -85,7 +82,7 @@ const CarPeerClient = () => {
                                   value=""
                                   required
                                 >
-                                  <option value="">Mecánico disponibles</option>
+                                  <option value="">Mecánicos disponibles</option>
                                   <option value="1">26131991 - José Sánchez</option>
                                   <option value="2">27556887 - Emmanuel Zambrano</option>
                                   <option value="3">25445796 - Ángel Vargas</option>
